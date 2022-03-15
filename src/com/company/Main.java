5345aabae36808ab2cb3738bec1cb3651dc14d9d
Kanhya <krushnabedge143@gmail.com>
2022-03-15 16:04:48 +0530

    message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,22 @@
+package com.company;
 
+import java.util.Scanner;
 
- class Main {
-
+public class Main {
+    public int AdditionOperation(int x,int y)
+    {
+        int z=x+y;
+        return z;
+    }
     public static void main(String[] args) {
-	    System.out.println("Hello World");
-
+        Main cal = new Main();
+        Scanner input=new Scanner(System.in);
+        int n1,n2,result;
+        System.out.println("enter num1");
+        n1=input.nextInt();
+        System.out.println("enter num2");
+        n2=input.nextInt();
+        result=cal.AdditionOperation(n1,n2);
+        System.out.println(result);
     }
 }
