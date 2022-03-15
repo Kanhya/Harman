78dbb51668b096b0738ebd04256235733c66537a
Kanhya <krushnabedge143@gmail.com>
2022-03-15 16:15:05 +0530

    division operation

---
+++ src/com/company/divisibility.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class divisibility {
+    public static void main(String[] args) {
+
+        Integer a=25;
+
+        if (a%5==0){
+            System.out.println(a+ " divisible by 5");
+        }
+        else{
+            System.out.println(a+ " not divisible by 5");
+        }
+
+    }
+
+}
