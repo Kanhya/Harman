95277b7133ea1429883eb5db0ac1fcc2de908612
Kanhya <krushnabedge143@gmail.com>
2022-03-17 14:54:12 +0530

    Filecreate

---
+++ src/com/company/Filecreat.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java. io. FileNotFoundException;
+import java. io. FileOutputStream;
+import java.io.IOException;
+public class Filecreat {
+public static void main(String[]args){
+        String data="welcome to Java Programming";
+        try{
+            FileOutputStream file = new FileOutputStream("Sample.txt");
+            byte b[]=data.getBytes();
+         file.write(b);
+            file.close();
+         System.out.println("file created successfully");
+        }
+        catch (FileNotFoundException e){
+
+            e.printStackTrace();
+        }
+        catch (IOException e){
+            System.out.println(e);
+        }
+        }
+        }
